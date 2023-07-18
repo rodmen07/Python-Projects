1dc83333b9a6ba8e88e364f5b034f21f5a248f41
Roderick Mendoza <rodmendoza07@gmail.com>
2023-07-18 12:59:37 -0700

    completed add and delete task

--- To Do List/app.py
+++ To Do List/app.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error while adding the task'
+
+    else:
+        tasks = Todo.query.all()
+        return render_template("index.html", tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error while deleting the task'
+
+# @app.route('/update/<int:id>', methods=['GET','POST'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
 
 if __name__ == "__main__":
     app.run(debug=True)
